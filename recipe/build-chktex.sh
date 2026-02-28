8939b4a4d318fe2fff182af5b487d0d71a9a4b2a
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-02-28 16:52:28 -0600

    try more build stuff

--- recipe/build-chktex.sh
+++ recipe/build-chktex.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-set -eux
+set -eux -o pipefail
+
+# https://github.com/conda-forge/chktex-feedstock/pull/8
+## maybe double-packed?
+ls configure || cd "${PKG_NAME}-${PKG_VERSION}" || echo "seems ok"
+
+ls configure
 
 # TODO: probably want pcre, but keep segfaulting with 8.44
 ./configure \
-    --prefix=$PREFIX \
+    "--prefix=${PREFIX}" \
     --disable-pcre
 
 make all
@@ -11,5 +17,5 @@ make all
 make install
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" && "${CROSSCOMPILING_EMULATOR:-}" == "" ]]; then
-make test
+    make test
 fi
