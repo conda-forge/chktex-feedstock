1bcac418378af41c3baba43eb2e676457db7ca4a
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-04-27 16:35:53 -0500

    more win

--- recipe/build-chktex.sh
+++ recipe/build-chktex.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 
-export CFLAGS="${CFLAGS:-} -I${PREFIX}/include -I${PREFIX}/include/ncurses -I${PREFIX}/include/ncursesw"
-export LDFLAGS="${LDFLAGS:-} -L${PREFIX}/lib -L${PREFIX}/lib/ncurses -L${PREFIX}/lib/ncursesw"
-export LIBS="${LIBS:-} -lncurses"
 
 # https://github.com/conda-forge/chktex-feedstock/pull/8
 ## maybe double-packed?
@@ -15,6 +12,9 @@ if [[ "${target_platform}" == "win-64" ]]; then
     ln -s "${PREFIX}/Library/usr/bin/perl.exe" "${PREFIX}/Library/usr/bin/perl5.exe"
 else
     ln -s "${PREFIX}/bin/perl" "${PREFIX}/bin/perl5"
+    export CFLAGS="${CFLAGS} -I${PREFIX}/include -I${PREFIX}/include/ncurses -I${PREFIX}/include/ncursesw"
+    export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -L${PREFIX}/lib/ncurses -L${PREFIX}/lib/ncursesw"
+    export LIBS="${LIBS} -lncurses"
 fi
 
 sed -E --in-place "s/install: chktex ChkTeX.dvi/install: chktex/" Makefile.in
