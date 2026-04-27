ef4a6892c32c4c2ee4d423f8fb040b6e17d154f4
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-04-27 10:29:24 -0500

    more build/test, patch perl shebang

--- recipe/build-chktex.sh
+++ recipe/build-chktex.sh
@@ -3,22 +3,33 @@ set -eux -o pipefail
 
 export CFLAGS="${CFLAGS} -I${PREFIX}/include -I${PREFIX}/include/ncurses -I${PREFIX}/include/ncursesw"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -L${PREFIX}/lib/ncurses -L${PREFIX}/lib/ncursesw"
+export LIBS="-lncurses"
 
 # https://github.com/conda-forge/chktex-feedstock/pull/8
 ## maybe double-packed?
 ls configure || cd "${PKG_NAME}-${PKG_VERSION}"
 
+cp COPYING "${SRC_DIR}/COPYING" || echo "COPYING already correct"
+
+ln -s "${PREFIX}/bin/perl" "${PREFIX}/bin/perl5"
+
+sed -iE "s/install: chktex ChkTeX.dvi/install: chktex/" Makefile.in
+
 ./configure --help
 
 # TODO: probably want pcre, but keep segfaulting with 8.44
 ./configure \
-    "--prefix=${PREFIX}" \
-    "--libdir=${PREFIX}/lib" \
+    --disable-pcre \
     "--includedir=${PREFIX}/include" \
-    --disable-pcre
+    "--libdir=${PREFIX}/lib" \
+    "--prefix=${PREFIX}"
 
 make all
 
 make install
+
+sed -i "s/perl5/perl/" "${PREFIX}/bin/deweb"
+
+rm "${PREFIX}/bin/perl5"
 
 make test
